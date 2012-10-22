9e711cc547fed3073b1789b949cb9a823a10e04e
Ravall <v.semenov@alt1.ru>
2012-10-22 11:36:07 +0400

    изменил хранение файлов э

--- sancta_pd/sancta_pd/config/route.py
+++
@@ -1,7 +0,0 @@
-def event(config):
-    config.add_route('event_list', '/event/list')
-    config.add_route('event_edit', '/event/edit/{id}')
-    config.add_route('event_add_icon', '/event/addicon/{id}')
-
-def routing(config):
-    event(config)
