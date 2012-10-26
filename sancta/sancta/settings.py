14b23040771ab6097366644233ba1382b135ea3d
Ravall <v.semenov@alt1.ru>
2012-10-26 17:53:18 +0400

    test api

--- sancta/sancta/settings.py
+++ sancta/sancta/settings.py
@@ -143,7 +143,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     # 'django.contrib.admindocs',
-    'mf_system'
+    'mf_system',
+    'api',
 )
 
 # A sample logging configuration. The only tangible logging
