012a7952f6909752b76bf6f6ee648011178fbad5
Ravall <v.semenov@alt1.ru>
2012-11-07 19:44:23 +0400

    заготовки

--- sancta/sancta/urls.py
+++ sancta/sancta/urls.py
@@ -1,29 +1,7 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
-
-
-
-admin.autodiscover()
-
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sancta.views.home', name='home'),
-    # url(r'^sancta/', include('sancta.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    url(r'^api/',include('api.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^restframework/', include('djangorestframework.urls', namespace='djangorestframework')),
 )
 
-
-if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-	urlpatterns += staticfiles_urlpatterns()
 
